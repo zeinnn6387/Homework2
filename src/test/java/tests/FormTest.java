eed504affbd3c3e68330646589c09cfba6aef016
smithjohn6387@gmail.com <smithjohn6387@gmail.com>
2020-11-18 06:01:12 +0300

    1

--- src/test/java/tests/FormTest.java
+++ src/test/java/tests/FormTest.java
@@ -1,12 +1,22 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FormTest {
+
+    @BeforeAll
+    static void setup() {
+        Configuration.startMaximized = true;
+    }
+
+
     @Test
     void testForm() {
         String firstName = "Константин",
@@ -18,16 +28,25 @@ public class FormTest {
                subjects ="",
                hobbies ="",
                picture = "",
-               currentAddress = "",
-
-
-
-
-
-
+               currentAddress = "улица Пушкина, дом Колотушкина",
+               state = "",
+               city = "";
 
 
         open("https://demoqa.com/automation-practice-form");
+
         $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        $("#firstName").val(firstName);
+        $("#lastName").val(lastName);
+        $("#userEmail").val(email);
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#userNumber").val(mobile);
+
+        //dateOfBirth
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__day--005").click();
+        $(".react-datepicker__year-select").selectOption("1994");
     }
 }
